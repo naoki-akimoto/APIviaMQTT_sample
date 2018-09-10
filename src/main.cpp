ad366caabb3ddcc7a016e3cfc0fa117ab38facbc
naoki.akimoto <naoki.akimoto@muraoka-design.com>
2018-09-10 12:18:25 +0900

    use define.

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,18 @@
 #include <unistd.h>
 #include <iostream>
 
+#define SITE ""
+#define PORT 1883
+#define APPID ""
+#define APPKEY ""
+#define VENDORTHINGID ""
+#define THINGPASSWORD ""
+
 int main() {
     MQTT_KiiAPI *kiiApi;
 
     kiiApi = new MQTT_KiiAPI(
-        "mqtt-us.kii.com",
-        1883,
-        "ldmaynmm98zp",
-        "75473e7bea444d42afedae4c4ecec64f",
-        "testVendor",
-        "testVendorPass");
+        SITE, PORT, APPID, APPKEY, VENDORTHINGID, THINGPASSWORD);
 
     while(getchar() != '\n') {
     }
